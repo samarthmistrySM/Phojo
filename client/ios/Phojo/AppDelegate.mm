088ca84353fd7492a4905149775c2004fffb8560
samarthmistrySM <g33ky.sam@gmail.com>
2025-01-19 21:04:23 +0530

    renamed the application

--- client/ios/Phojo/AppDelegate.mm
+++ client/ios/Phojo/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"MyApp2";
+  self.moduleName = @"Phojo";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- client/ios/PhojoTests/PhojoTests.m
+++ client/ios/PhojoTests/PhojoTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyApp2Tests : XCTestCase
+@interface PhojoTests : XCTestCase
 
 @end
 
-@implementation MyApp2Tests
+@implementation PhojoTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
