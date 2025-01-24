6f83c11d7c32ce55cf31f4a802678978bf48a734
samarthmistrySM <g33ky.sam@gmail.com>
2025-01-24 22:27:20 +0530

    removing unwanted files

--- client/src/secrets.js
+++
@@ -1,9 +0,0 @@
-const secrets = {
-    API_URL: "http://localhost:4000/api",
-    CloudinaryUploadPreset: "gbxinum8",
-    CloudinaryCloudName: "pinorama",
-    CloudinaryApiUrl: "https://api.cloudinary.com/v1_1/pinorama/image/upload",
-    // API_URL: "https://phojo.vercel.app/api",
-};
-
-export default secrets;
